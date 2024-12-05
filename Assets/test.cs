1d0c03f6cb602957eb251e0f49f814973578c96a
jorodane <jorodane@naver.com>
2024-12-05 16:26:43 +0900

    테스트 수정

--- Assets/test.cs
+++ Assets/test.cs
@@ -7,7 +7,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // git에 올릴 정보
     }
 
     // Update is called once per frame
